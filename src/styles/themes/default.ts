757d714f2a3561f224740e9f7404ca45e69132c2
Davi Lima <davi.rlima04@gmail.com>
2022-09-29 18:01:53 -0400

    setup

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,30 @@
+export const defaultTheme = {
+  color: {
+    blue: " #3294f8",
+
+    "base-background": "#071422",
+
+    "base-title": " #e7edf4",
+    "base-subtitle": "#c4d4e3",
+    "base-text": "#afc2d4",
+
+    "base-span": "#7b96b2",
+    "base-input": "#040f1a",
+    "base-label": " #3a536b",
+    "base-border": "#1c2f41",
+
+    "base-post": "#112131",
+    "base-profile": "#0b1b2b",
+  },
+
+  text: {
+    "title-large": "1.5rem",
+    "tilte-medium": "1.25rem",
+    "title-short": "1.125rem",
+
+    "text-medium": "1rem",
+    "text-short": "0.875rem",
+
+    link: "0.75rem",
+  },
+} as const;
