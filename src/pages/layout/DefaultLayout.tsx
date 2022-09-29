64a6fe455aeb1eb433a41435546b1e2b97236d83
Davi Lima <davi.rlima04@gmail.com>
2022-09-29 18:15:22 -0400

    configurando rotas

---
+++ src/pages/layout/DefaultLayout.tsx
@@ -0,0 +1,13 @@
+import { Header } from "../../components/Header";
+import { Outlet } from "react-router-dom";
+
+export function DefaultLayout() {
+  return (
+    <>
+      <Header />
+      <main>
+        <Outlet />
+      </main>
+    </>
+  );
+}
